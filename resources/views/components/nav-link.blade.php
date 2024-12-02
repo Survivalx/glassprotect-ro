57a612c109daef8989f92f0aa46341bfe0f1e105
Survivalx <goshaz7c@icloud.com>
2024-12-02 14:46:26 +0200

    nav-link costumization

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -12,9 +12,9 @@ $classes = ($active ?? false)
 
 
 @props(['active' => false, 'type' => 'a'])
-
+{{-- bg-indigo-500 -> is active(); --}}
 @if ($type == 'a')
-    <a class="{{ $active ? 'bg-indigo-500 text-white' : 'text-slate-950 hover:bg-indigo-100 hover:text-black transition-all' }} rounded-md px-3 py-2 text-sm font-medium"
+    <a class="{{ $active ? 'bg-gradient-to-bl from-indigo-300 to-indigo-700 text-white' : 'text-slate-950 hover:bg-indigo-100 hover:text-black transition-all' }} rounded-md px-3 py-2 text-sm font-medium"
         aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
 @else
     <button
