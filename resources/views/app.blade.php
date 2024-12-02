22c8d0b765eb5cc1fd589642f59d286fcc5796f0
Survivalx <goshaz7c@icloud.com>
2024-12-02 14:48:17 +0200

    edits

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
     <h1 inert class="text-3xl">Home page test font here please</h1>
+
 </x-layout>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,18 +1,4 @@
-{{-- @props(['active'])
-
-@php
-$classes = ($active ?? false)
-            ? 'bg-red-500'
-            : 'hover:bg-sky-600 hover:text-white leading-normal transition-all px-3 py-2 rounded-lg font-bold text-black';
-@endphp
-
-<a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
-</a> --}}
-
-
 @props(['active' => false, 'type' => 'a'])
-{{-- bg-indigo-500 -> is active(); --}}
 @if ($type == 'a')
     <a class="{{ $active ? 'bg-gradient-to-bl from-indigo-300 to-indigo-700 text-white' : 'text-slate-950 hover:bg-indigo-100 hover:text-black transition-all' }} rounded-md px-3 py-2 text-sm font-medium"
         aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
