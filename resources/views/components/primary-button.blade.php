00be15b5a874101c860d12708695476dd77cf80d
Survivalx <goshaz7c@icloud.com>
2024-12-24 17:01:37 +0200

    asdads

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,4 +1,4 @@
 <button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'py-2 leading-150 px-4 font-light bg-blue-600 text-white rounded-sm  transition-all hover:shadow-xl dark:hover:shadow-blue-800/25 hover:shadow-blue-100']) }}>
+{{ $attributes->merge(['type' => 'submit', 'class' => 'py-2 leading-150 px-4 font-light bg-blue-600 text-white rounded-sm  transition-all hover:shadow-xl dark:hover:shadow-blue-800/25 hover:shadow-blue-100']) }}>
     {{ $slot }}
 </button>
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
             <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-200">Page not found</h2>
             <p class="text-lg text-gray-800 dark:text-gray-200">Sorry, the page you are looking for, could not be found.
             </p>
-            <x-primary-button class="mt-5" href="{{ route('home') }}">Go back to Home</x-primary-button>
+            <a class="mt-5 py-2 leading-150 px-4 font-light bg-blue-600 text-white rounded-sm  transition-all hover:shadow-xl dark:hover:shadow-blue-800/25 hover:shadow-blue-100" href="{{ route('home') }}">Go back to Home</a>
         </div>
     </div>
 </x-layout>
